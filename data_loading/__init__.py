c06d2bfb23bd8352caf13f5c8b897571398dbeed
DragonMoffon <happydog.aja@gmail.com>
2024-05-02 00:31:37 +1200

    quickly adding `open`  kwargs to the method returned by `make_package_file_loader`

--- data_loading/__init__.py
+++ data_loading/__init__.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from contextlib import contextmanager
 import importlib.resources as pkg_resources
 
@@ -8,14 +9,53 @@ __all__ = (
 )
 
 
-def make_package_file_loader(package, data_type: str):
+def make_package_file_loader(
+        package,
+        data_type: str,
+        mode: str = "r",
+        buffering: int = -1,
+        encoding: str | None = None,
+        errors: str | None = None,
+        newline: str | None = None,
+        closefd: bool = True,
+        opener: Callable[[str, int], int] | None = None
+):
+    """
+    Create a reusable function for opening files of a particular type at a particular location
+    Can also set new defaults for the `open` function used internally.
+    
+    See `open` for other arguments
+
+    :param package: the location to open at, as a package str.
+    :param data_type: the type to put as the `.type` for a file
+    :return: A function which creates a context manager around an opened file
+    """
+
     @contextmanager
-    def _file_loader(name: str):
+    def _file_loader(
+            name: str,
+            _mode: str = mode,
+            _buffering: int = buffering,
+            _encoding: str | None = encoding,
+            _errors: str | None = errors,
+            _newline: str | None = newline,
+            _closefd: bool = closefd,
+            _opener: Callable[[str, int], int] | None = opener
+    ):
+        """
+        Open a file with a predetermined type and location with given name.
+        Also has the arguments for `open` available. Custom defaults can be provided
+        at the same time as the type and location
+
+        See `open` for other arguments
+
+        :param name: The name of the file to open (without .type) at the end.
+        """
         file_name = f"{name}.{data_type}"
         file = None
         try:
             with pkg_resources.path(package, file_name) as path:
-                file = open(path)
+                file = open(path, _mode, _buffering, _encoding, _errors, _newline, _closefd, _opener)
             yield file
         finally:
             if file is not None:
