858e4249c8a12c7d80b6b684982861d24f7563c8
DragonMoffon <happydog.aja@gmail.com>
2024-05-06 22:21:01 +1200

    animatable proxy is so cursed

--- animator/animator.py
+++ animator/animator.py
@@ -1,4 +1,6 @@
-from typing import Callable, Iterable, TypeVar, Protocol
+from typing import Callable, Iterable, TypeVar, Protocol, Generic
+
+import _weakref
 
 from animator.lerp import ease_linear
 
@@ -17,6 +19,61 @@ class Animatable(Protocol):
 
 T = TypeVar('T')
 A = TypeVar('A', bound=Animatable)
+
+
+class AnimatableProxy(Generic[T]):
+
+    def __init__(self, obj: T, anim_speed: float, animated_attributes: tuple[str, ...]):
+        self.__dict__['_core'] = obj
+        self.__dict__['_time'] = 0.0
+        self.__dict__['_speed'] = anim_speed
+        self.__dict__['_attr'] = set(animated_attributes)
+        self.__dict__['_old_values'] = {attr: getattr(obj, attr) for attr in animated_attributes}
+        self.__dict__['_target_values'] = {attr: getattr(obj, attr) for attr in animated_attributes}
+        self.__dict__['_start_time'] = {attr: 0.0 for attr in animated_attributes}
+        self.__dict__['_end_time'] = {attr: 0.0 for attr in animated_attributes}
+
+    def __setattr__(self, key, value):
+        old = getattr(self.__dict__['_core'], key)
+
+        self.__dict__['_old_values'][key] = old
+        self.__dict__['_target_values'][key] = value
+        self.__dict__['_start_time'][key] = self.__dict__['_time']
+        self.__dict__['_end_time'][key] = self.__dict__['_time'] + self.__dict__['_speed']
+
+    def __getattr__(self, item):
+        return getattr(self.__dict__['_core'], item)
+
+    def update(self, dt):
+        self.__dict__['_time'] = time = self.__dict__['_time'] + dt
+        old = self.__dict__['_old_values']
+        target = self.__dict__['_target_values']
+        start = self.__dict__['_start_time']
+        end = self.__dict__['_end_time']
+
+        for attr in self.__dict__['_attr']:
+            lerp = ease_linear(
+                old[attr], target[attr],
+                start[attr], end[attr],
+                time
+            )
+            setattr(self.__dict__['_core'], attr, lerp)
+
+
+class DragonAnimator(Generic[T]):
+
+    def __init__(self, animated_attributes: tuple[str, ...]):
+        self._proxies: list[AnimatableProxy[T]] = list()
+        self._attrs = animated_attributes
+
+    def proxy(self, obj: T) -> AnimatableProxy[T]:
+        proxy = AnimatableProxy(obj, 1.0, self._attrs)
+        self._proxies.append(proxy)
+        return proxy
+
+    def update(self, dt: float):
+        for proxy in self._proxies:
+            proxy.update(dt)
 
 
 class Animator:
--- animator/main.py
+++ animator/main.py
@@ -1,22 +1,23 @@
 from arcade import Window
 from arcade import SpriteSolidColor
 
-from animator.animator import Animator
+from animator.animator import Animator, DragonAnimator
 
 
 class AnimWindow(Window):
     def __init__(self):
         super().__init__(1280, 720, "Animator Test")
 
-        self.sprite = SpriteSolidColor(100, 100, 100, self.center_y)
-        self.animator = Animator([self.sprite], "center_x")
+        self.a_b: DragonAnimator[SpriteSolidColor] = DragonAnimator(("center_x", "center_y"))
+        self.sprite = self.a_b.proxy(SpriteSolidColor(100, 100, 100, self.center_y))
+
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         print(self.sprite.__dict__)
         self.sprite.center_x = self.width - 100
 
     def on_update(self, delta_time: float):
-        self.animator.update(delta_time)
+        self.a_b.update(delta_time)
         return super().on_update(delta_time)
 
     def on_draw(self):
