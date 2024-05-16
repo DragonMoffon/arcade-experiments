d02b31d1552e4637919c3ef7eaf2d5e612a96cb7
DigiDuncan <digiduncan@gmail.com>
2024-05-16 05:08:42 -0400

    "new" project

---
+++ heightviz/render.py
@@ -0,0 +1,48 @@
+from PIL import Image
+
+from arcade import gl
+from arcade import get_window
+
+from common.data_loading import make_package_string_loader, make_package_path_finder
+import sphere.data as data
+
+get_shader_string = make_package_string_loader(data, 'glsl')
+get_img_path = make_package_path_finder(data, "png")
+
+
+class Renderer:
+
+    def __init__(self):
+        self._ctx = ctx = get_window().ctx
+
+        self._sphere = gl.geometry.sphere(360, 1024, 1024)
+        self._texture_program = ctx.program(
+            vertex_shader=get_shader_string("sphere_texture_vs"),
+            fragment_shader=get_shader_string("sphere_texture_fs")
+        )
+        self._texture_program["wrldText"] = 0
+        self._texture_program["elevText"] = 1
+        img = Image.open(get_img_path("world_oct"))
+        self._world_texture = ctx.texture(img.size, components=3, data=img.tobytes())
+        img = Image.open(get_img_path("world_bump"))
+        self._elev_texture = ctx.texture(img.size, components=4, data=img.tobytes())
+
+        self._star_sphere = gl.geometry.sphere(20000, 64, 64)
+        self._star_texture_program = ctx.program(
+            vertex_shader=get_shader_string("blank_sphere_texture_vs"),
+            fragment_shader=get_shader_string("blank_sphere_texture_fs")
+        )
+        img = Image.open(get_img_path("stars"))
+        self._star_texture = ctx.texture(img.size, components=1, data=img.tobytes())
+
+    def draw(self):
+        self._ctx.disable(self._ctx.CULL_FACE)
+        self._ctx.enable(self._ctx.DEPTH_TEST)
+
+        self._world_texture.use(0)
+        self._elev_texture.use(1)
+        self._sphere.render(self._texture_program)
+
+    def star_draw(self):
+        self._star_texture.use(0)
+        self._star_sphere.render(self._star_texture_program)
---
+++ heightviz/window.py
@@ -0,0 +1,125 @@
+import math
+
+from arcade import Window, camera, key
+from pyglet.math import Vec2, Vec3
+
+from common.util import clamp
+
+from sphere.render import Renderer
+
+
+class App(Window):
+
+    def __init__(self):
+        super().__init__(1280, 720, "3D Sphere")
+
+        self._renderer = Renderer()
+        self._camera_data = camera.CameraData(
+            position=(0.0, 0.0, 0.0)
+        )
+        self._projection_data = camera.PerspectiveProjectionData(
+            self.width / self.height,
+            90,
+            100.0,
+            20000.0,
+            self.ctx.viewport
+        )
+
+        self._camera = camera.PerspectiveProjector(view=self._camera_data, projection=self._projection_data)
+        self._camera_2 = camera.OrthographicProjector(view=self._camera_data)
+        self._camera_2.projection.near = 0.01
+        self._camera_2.projection.far = 200000.0
+
+        self.forward = 0
+        self.horizontal = 0
+        self.vertical = 0
+
+        self._lat = 0
+        self._long = 0
+        self._radius = 7500
+
+        self.look_at_center()
+
+        self._is_in_center_view_mode = True
+
+    def look_at_center(self):
+        y = math.sin(self._lat)
+        x = math.cos(-self._long) * math.cos(self._lat)
+        z = math.sin(-self._long) * math.cos(self._lat)
+        forward = (-x, -y, -z)
+        pos = (x * self._radius, y * self._radius, z * self._radius)
+
+        self._camera_data.position = pos
+        self._camera_data.forward = forward
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        match symbol:
+            case key.W:
+                self.forward += 1
+            case key.S:
+                self.forward -= 1
+            case key.D:
+                self.horizontal += 1
+            case key.A:
+                self.horizontal -= 1
+            case key.SPACE:
+                self.vertical += 1
+            case key.LSHIFT:
+                self.vertical -= 1
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        match symbol:
+            case key.W:
+                self.forward -= 1
+            case key.S:
+                self.forward += 1
+            case key.D:
+                self.horizontal -= 1
+            case key.A:
+                self.horizontal += 1
+            case key.SPACE:
+                self.vertical -= 1
+            case key.LSHIFT:
+                self.vertical += 1
+
+    def on_draw(self):
+        self.clear()
+        with self._camera.activate():
+            self._renderer.star_draw()
+
+        with self._camera_2.activate():
+            self._renderer._texture_program['light'] = self._camera_data.forward
+            self._renderer.draw()
+
+    def on_update(self, delta_time: float):
+        if self._is_in_center_view_mode:
+            if self.vertical:
+                self._radius = clamp(7000, self._radius + self.vertical * 1000.0 * delta_time, 10000)
+
+            if self.horizontal:
+                self._long = (self._long + (1 + self.horizontal * delta_time) * math.pi) % (2 * math.pi) - math.pi
+
+            if self.forward:
+                self._lat = clamp(-math.pi / 2.0, self._lat + self.forward * math.pi * delta_time, math.pi / 2.0)
+
+            if self.forward or self.horizontal or self.vertical:
+                self.look_at_center()
+
+        else:
+            if self.forward:
+                velocity = self.forward * 100.0 * delta_time
+                old_pos = self._camera_data.position
+                fwd = self._camera_data.forward
+
+                new_pos = old_pos[0] + fwd[0] * velocity, old_pos[1] + fwd[1] * velocity, old_pos[2] + fwd[2] * velocity
+                self._camera_data.position = new_pos
+
+            if self.horizontal or self.vertical:
+                direction = Vec2(self.horizontal, self.vertical).normalize()
+                new_pos = camera.grips.strafe(self._camera_data, direction)
+                self._camera_data.position = new_pos
+
+
+def main():
+    app = App()
+    app.run()
