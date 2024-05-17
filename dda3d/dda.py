d655be792d16820fc88ea789d95316d6feea56fa
DragonMoffon <happydog.aja@gmail.com>
2024-05-17 22:23:52 +1200

    more docs for digi

--- dda3d/dda.py
+++ dda3d/dda.py
@@ -21,27 +21,27 @@ class DDAResult(NamedTuple):
 
 
 def dda(x: float, y: float, z: float, d_x: float, d_y: float, d_z: float, bounds_x: int, bounds_y: int, bounds_z: int) -> DDAResult:
-    i_x, i_y, i_z = int(floor(x)), int(floor(y)), int(floor(z))  # grid indices
+    i_x, i_y, i_z = int(floor(x)), int(floor(y)), int(floor(z))  # grid indices (index coord)
 
-    # distance traveled when moving 1 unit along the x, y, and z axis
+    # distance traveled when moving 1 unit along the x, y, and z axis (change in parametric value t at coord)
     dt_x = float('inf') if d_x == 0 else 1.0 / abs(d_x)
     dt_y = float('inf') if d_y == 0 else 1.0 / abs(d_y)
     dt_z = float('inf') if d_z == 0 else 1.0 / abs(d_z)
 
-    # distance traveled when moving to the edge of the starting pos along the x, y, and z axis.
+    # distance traveled when moving to the edge of the starting pos along the x, y, and z axis. (parametric value t at coord)
     t_x = ((1.0 - (x % 1)) if d_x >= 0.0 else (x % 1)) * dt_x
     t_y = ((1.0 - (y % 1)) if d_y >= 0.0 else (y % 1)) * dt_y
     t_z = ((1.0 - (z % 1)) if d_z >= 0.0 else (z % 1)) * dt_z
 
-    # the index step when moving along the x, y, and z axis.
+    # the index step when moving along the x, y, and z axis. (start_coord)
     s_x = 1 if d_x >= 0.0 else -1
     s_y = 1 if d_y >= 0.0 else -1
     s_z = 1 if d_z >= 0.0 else -1
 
     t = 0.0
     t_o = 0.0
-    n_x, n_y, n_z = i_x, i_y, i_z
-    l_x, l_y, l_z = i_x, i_y, i_z
+    n_x, n_y, n_z = i_x, i_y, i_z  # (next coord)
+    l_x, l_y, l_z = i_x, i_y, i_z  # (last coord)
     path = []
     while True:
         if t_x < min(t_y, t_z):
