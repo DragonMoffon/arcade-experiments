4ca49285023c76ca409c397189f3c0fd1cedbe39
DigiDuncan <digiduncan@gmail.com>
2024-05-25 17:27:11 -0400

    polish, pass 1
    text needs work

--- __main__.py
+++ __main__.py
@@ -8,6 +8,7 @@ from multi_variable_sorting.sorting import main as mvs_main
 from animator.main import main as animator_main
 from sphere.window import main as sphere_main
 from heightviz.window import main as height_main
+from heightviz2d.window import main as height2d_main
 
 if __name__ == '__main__':
     load_shared_font("gohu")
@@ -19,6 +20,7 @@ if __name__ == '__main__':
             "Multi-Variable Sorting": (mvs_main, (), {}),
             "Automatic Animator": (animator_main, (), {}),
             "Sphere 3D": (sphere_main, (), {}),
-            "Scale Visualizer": (height_main, (), {})
+            "Scale Visualizer 2D": (height2d_main, (), {}),
+            "Scale Visualizer 3D": (height_main, (), {})
         }
     )
--- heightviz2d/window.py
+++ heightviz2d/window.py
@@ -5,10 +5,22 @@ import random
 import arcade
 
 from common.data_loading import make_package_file_opener, make_package_path_finder
+from common.util import load_shared_sound
 import heightviz2d.data as data
 
 open_json_file = make_package_file_opener(data, "json")
 get_png_path = make_package_path_finder(data, "png")
+
+
+def cm_to_str(cm: float) -> str:
+    if cm < 1:
+        return f"{cm * 10:.1f}mm"
+    elif cm < 100:
+        return f"{cm:.1f}cm"
+    elif cm < 100000:
+        return f"{cm / 100:.1f}m"
+    else:
+        return f"{cm / 100000:.1f}km"
 
 
 class ZoomBucket:
@@ -41,6 +53,7 @@ class ZoomBucket:
             sprite.depth = -scale
             sprite.width = scale * aspect
             sprite.center_y = sprite.height / 2.0
+            sprite.properties["name"] = name
 
             self.zoom_levels[level].append(sprite)
             sprites.append(sprite)
@@ -74,11 +87,37 @@ class HeightViz2DWindow(arcade.Window):
         super().__init__()
         self._zoom_factor = 0
 
-        self._cam = arcade.camera.Camera2D(position=(0.0, 0.0), far=100001)
+        self._cam = arcade.camera.Camera2D(position=(0.0, 0.0), far=10000001)
 
         self._zoom_buckets = ZoomBucket()
         with open_json_file('scales') as file:
             self._zoom_buckets.load_items(json.load(file))
+
+        self.selected_box: tuple[float, float, float, float] | None = None
+        self.selected_name: str = ""
+        self.selected_size: float = 0
+
+        self.beep = load_shared_sound("blip_c")
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        pass
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        old_selected_name = self.selected_name
+        x, y, _ = self._cam.unproject((x, y))
+        for sprite in sorted(self._zoom_buckets._sprites, key = lambda s: s.height):
+            if (sprite.left <= x <= sprite.right) and (sprite.bottom <= y <= sprite.top):
+                self.selected_box = (sprite.left, sprite.right, sprite.bottom, sprite.top)
+                self.selected_name = sprite.properties["name"]
+                self.selected_size = sprite.height * (10 ** (self._zoom_buckets.current_focus_level * 2))
+
+                if self.selected_name != old_selected_name:
+                    self.beep.play()
+                break
+        else:
+            self.selected_box = None
+            self.selected_name = ""
+            self.selected_size = 0
 
     def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
         if scroll_y > 0:
@@ -106,9 +145,20 @@ class HeightViz2DWindow(arcade.Window):
 
     def on_draw(self):
         self.ctx.enable(self.ctx.DEPTH_TEST)
-        self.clear(color=(155, 155, 155, 155))
+        self.clear(color=(20, 20, 20))
         self._cam.use()
         self._zoom_buckets.draw()
+
+        if self.selected_box:
+            arcade.draw_lrbt_rectangle_outline(*self.selected_box, arcade.color.WHITE_SMOKE, 3 / self._cam.zoom)
+            arcade.draw_text(self.selected_name.title() + "\n" + cm_to_str(self.selected_size),
+                             self.selected_box[1], self.selected_box[3],
+                             arcade.color.WHITE_SMOKE,
+                             anchor_x = "left", anchor_y = "top",
+                             font_name="GohuFont 11 Nerd Font Mono",
+                             font_size = max(18 / (self._cam.zoom), 18),
+                             multiline = True,
+                             width = 1000)
 
 
 def main():
