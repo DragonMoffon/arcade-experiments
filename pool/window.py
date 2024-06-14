6d57b1cd8079c5f1b318f5d3b4e5933fe4896b49
DragonMoffon <happydog.aja@gmail.com>
2024-06-14 20:01:28 +1200

    pool :3

--- pool/window.py
+++ pool/window.py
@@ -1,4 +1,5 @@
 from random import random, choices
+from typing import Callable
 
 from arcade.experimental.input import MouseButtons, Keys
 from arcade import (
@@ -12,22 +13,45 @@ from arcade import (
     draw_rect_outline,
     draw_rect_filled,
     draw_circle_outline,
+    draw_lines,
     draw_point,
-    MOUSE_BUTTON_RIGHT
+    MOUSE_BUTTON_LEFT,
+    load_texture
 )
 
 from common.util import ProceduralAnimator, load_shared_sound
+from common.data_loading import make_package_path_finder
 from common.experimentwindow import ExpWin
 
 from pool.pool import Pool
+import pool.data as data
 
+get_png_path = make_package_path_finder(data, 'png')
+get_texture = lambda name: load_texture(get_png_path(name))
 
 SPRITE_COUNT = 100
 ANIMATOR_COUNT = 20
 
+SPEEDS = (100, 1000)
+
 RESPAWN_TIME = 1.0
 
 GRAVITY_MAX = 200.0
+
+LANE_FRACTION = 1.0 / 4.0
+
+
+def generate_notes():
+    t = 0.0
+    lanes = (0, 1, 2, 3)  # lanes 1-4 with 5 being a note-speed event
+    weights = (1.0, 1.0, 1.0, 1.0)
+    loop = True
+    yield 0, t
+    while loop:
+        lane = choices(lanes, weights)[0]
+        t += 0.5
+        loop = (yield lane, t)
+    yield None
 
 
 def pick_point_in_rect(rect: Rect) -> tuple[float, float]:
@@ -36,6 +60,7 @@ def pick_point_in_rect(rect: Rect) -> tuple[float, float]:
 
 def pick_point_along_rect(rect: Rect, d_x: float, d_y: float) -> tuple[float, float]:
     along_y = choices((True, False), weights=(abs(d_x), abs(d_y)))
+    print(along_y)
     if along_y:
         x = rect.left + random() * rect.width
         if d_y > 0:
@@ -55,8 +80,18 @@ def pick_point_along_rect(rect: Rect, d_x: float, d_y: float) -> tuple[float, fl
 class PoolWindow(ExpWin):
 
     def __init__(self, show_fps: bool):
-        super().__init__(1280, 720)
+        super().__init__(1280, 720, update_rate=1/1000)
+        self._note_gen = generate_notes()
+
         self.show_fps(show_fps)
+
+        self._textures = (
+            get_texture('normal-1'),
+            get_texture('normal-2'),
+            get_texture('normal-3'),
+            get_texture('normal-4'),
+            get_texture('default')
+        )
 
         self._control_sound: Sound = load_shared_sound('blip_e')
         self._deposit_sound: Sound = load_shared_sound('blip_a')
@@ -68,68 +103,102 @@ class PoolWindow(ExpWin):
         self._deposit_bounds: Rect = XYWH(200, self.center_y, 150, 350)
         self._deposit_visual: Rect = XYWH(200, self.center_y, 200, 400)
 
-        self._gravity: float = 100.0
-        self._gravity_dir: tuple[float, float] = 0.0, -1.0
+        self._note_speed: float = 100.0
 
-        sprites = list(SpriteSolidColor(50, 50, *pick_point_in_rect(self._deposit_bounds)) for _ in range(SPRITE_COUNT))
+        sprites = list(Sprite(self._textures[-1], 0.2, *pick_point_in_rect(self._deposit_bounds)) for _ in range(SPRITE_COUNT))
         self._sprites: SpriteList = SpriteList()
         self._sprites.extend(sprites)
         self._sprite_pool: Pool[Sprite] = Pool(sprites)
 
-        self._respawn_timer: float = 0.0
+        self._active_picker: Callable[[float, float, float, float], None] = None
 
-        self._slider_rect: Rect = XYWH(self.width-200, self.center_y - GRAVITY_MAX/2.0 - 50, 10, GRAVITY_MAX+10)
-        self._slider_text: Text = Text(f'{self._gravity}', self._slider_rect.right + 10, self._slider_rect.y, anchor_x='left', anchor_y='center')
+        self._t = 0.0
 
-    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
-        button = MouseButtons(button)
-        match button:
-            case MouseButtons.LEFT:
-                if self._fall_bounds.point_in_rect((x, y)) and self._sprite_pool.has_free_slot():
-                    sprite = self._sprite_pool.get()
-                    sprite.position = (x, y)
-                    self._control_sound.play()
+        self._next_note = next(self._note_gen)
+        self._next_speed_time = self._t + 5.0
+        self._next_note_speed: float = 100 + (900 - (self._note_speed - 100))
+        self._t_offset: float = (self._fall_bounds.height + 25) / self._note_speed
 
-    def on_mouse_drag(self, x: int, y: int, dx: int, dy: int, buttons: int, modifiers: int):
-        if buttons & MOUSE_BUTTON_RIGHT:
-            self._gravity = max(0.0, min(GRAVITY_MAX, self._gravity + dy))
-            self._slider_text.text = f'{self._gravity}'
-            self._control_sound.play(0.1)
+        self._time_text: Text = Text(f'{self._t: .3f}', x=self._fall_bounds.right + 5, y=self._fall_bounds.y,
+                                     anchor_x='left', anchor_y='center')
 
     def on_update(self, delta_time: float):
         # `gravity` here is just velocity not acceleration, but eh we could use the sprite's change x and y ;-;
-        g = self._gravity * delta_time
-        gravity = self._gravity_dir[0] * g, self._gravity_dir[1] * g
-        for sprite in self._sprite_pool.given_items:
-            o_pos = sprite.position
-            n_pos = sprite.position = o_pos[0] + gravity[0], o_pos[1] + gravity[1]
+        self._t += delta_time
+        self._time_text.text = f'{self._t: .3f}'
 
-            if not self._fall_cut_bounds.point_in_rect(n_pos):
+        next_speed_y = self._fall_bounds.bottom + (self._next_speed_time - self._t) * self._note_speed
+        if next_speed_y < self._fall_cut_bounds.top:
+            # All notes that get spawned after the note speed change should account for it
+            top_t = self._next_speed_time + (self._fall_cut_bounds.top - next_speed_y) / self._next_note_speed
+        else:
+            # All notes that get spawned before the note speed change should use the note_speed
+            top_t = self._t + (self._fall_cut_bounds.top - self._fall_bounds.bottom) / self._note_speed
+
+        while top_t >= self._next_note[-1] and self._sprite_pool.has_free_slot():
+            lane, t = self._next_note
+            sprite = self._sprite_pool.get()
+            x = self._fall_bounds.left + LANE_FRACTION * self._fall_bounds.width * (0.5 + lane)
+            if t < self._next_speed_time:
+                y = self._fall_bounds.bottom + (t - self._t) * self._note_speed
+            else:
+                y = next_speed_y + (t - self._next_speed_time) * self._next_note_speed
+
+            sprite.texture = self._textures[lane]
+            sprite.position = x, y
+            sprite.t = t
+            # self._get_sound.play()
+            self._next_note = self._note_gen.send(True)
+
+        for sprite in self._sprite_pool.given_items:
+            t = sprite.t
+            if t < self._next_speed_time:
+                y = self._fall_bounds.bottom + (t - self._t) * self._note_speed
+            else:
+                y = next_speed_y + (t - self._next_speed_time) * self._next_note_speed
+            sprite.center_y = y
+
+            if y <= self._fall_bounds.bottom:
                 self._sprite_pool.give(sprite)
                 sprite.position = pick_point_in_rect(self._deposit_bounds)
-                self._deposit_sound.play()
+                sprite.texture = self._textures[-1]
+                self._deposit_sound.play(0.05)
+                print(f'{(self._t - sprite.t) * 1000: .0f}')
 
-        self._respawn_timer += delta_time
-        if self._respawn_timer >= RESPAWN_TIME:
-            self._respawn_timer -= RESPAWN_TIME
-
-            if self._sprite_pool.has_free_slot():
-                sprite = self._sprite_pool.get()
-                sprite.position = pick_point_along_rect(self._fall_bounds, *self._gravity_dir)
-                self._get_sound.play()
+        if self._t >= self._next_speed_time:
+            print('switched')
+            self._next_speed_time += 15.0
+            self._note_speed = self._next_note_speed
+            self._next_note_speed = 100 + (900 - (self._note_speed - 100))
+            self._t_offset = (self._fall_bounds.height + 25) / self._note_speed
 
     def on_draw(self):
         self.clear()
 
         draw_rect_outline(self._fall_bounds, (255, 255, 255, 255))
-        draw_rect_outline(self._fall_cut_bounds, (255, 0, 0, 255))
+        next_speed_y = self._fall_bounds.bottom + (self._next_speed_time - self._t) * self._note_speed
+        draw_lines(
+            (
+                (self._fall_cut_bounds.left, self._fall_cut_bounds.top), (self._fall_cut_bounds.right, self._fall_cut_bounds.top),
+                (self._fall_cut_bounds.left, self._fall_cut_bounds.bottom), (self._fall_cut_bounds.right, self._fall_cut_bounds.bottom),
+                (self._fall_cut_bounds.left, next_speed_y), (self._fall_cut_bounds.right, next_speed_y)
+            ),
+            (255, 0, 0, 255)
+        )
+        l = self._fall_bounds.left
+        w = self._fall_bounds.width
+        draw_lines(
+            (
+                (l + LANE_FRACTION * w, self._fall_bounds.top), (l + LANE_FRACTION * w, self._fall_bounds.bottom),
+                (l + LANE_FRACTION * 2 * w, self._fall_bounds.top), (l + LANE_FRACTION * 2 * w, self._fall_bounds.bottom),
+                (l + LANE_FRACTION * 3 * w, self._fall_bounds.top), (l + LANE_FRACTION * 3 * w, self._fall_bounds.bottom),
+            ),
+            (100, 100, 100, 255)
+        )
         draw_rect_outline(self._deposit_visual, (255, 255, 255, 255))
 
-        draw_rect_filled(self._slider_rect, (100, 100, 100, 255))
-        draw_point(self._slider_rect.x, self._slider_rect.bottom + 5 + self._gravity, (255, 255, 255, 255), 10)
-        self._slider_text.draw()
-
         self._sprites.draw(pixelated=True)
+        self._time_text.draw()
 
 
 def main(show_fps: bool):
