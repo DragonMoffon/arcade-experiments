3778c1bc265130b6ea062ea3561eefb4023b0444
DragonMoffon <happydog.aja@gmail.com>
2024-08-06 21:22:20 +1200

    nutka

--- wavrider/__main__.py
+++ wavrider/__main__.py
@@ -1,4 +1,6 @@
 # nuitka-project: --enable-plugin=tk-inter
+# nuitka-project: --include-package-data=common
+# nuitka-project: --force-stderr-spec=err.txt
 from wavrider.window import main
 
 if __name__ == '__main__':
